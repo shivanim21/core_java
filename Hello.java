563c2da78687f4e9790153b80082442756c56e6d
shivanim21 <shivanimaddala212@gmail.com>
2025-01-22 22:59:10 +0530

    primitive data types, variables and literals

--- Hello.java
+++ Hello.java
@@ -1,6 +1,18 @@
 public class Hello {
   public static void main(String[] args) {
     //System.out.println("Hello World");
-    System.out.println(3+4);
+    //System.out.println(3+4);
+
+    //Primitive Data Types and variables and literals
+    int num = 21;
+    byte b = 127;
+    short s = 345;
+    long l = 6789l;
+    float f = 5.8f;
+    double d = 5.8;
+    char c = 'c';
+    boolean bool = true;
+
+    double db = 10_00_00_000;
   }
 }
