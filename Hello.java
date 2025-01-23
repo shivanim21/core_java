dbabcc234416290cbae76cff9266a8178e46c039
shivanim21 <shivanimaddala212@gmail.com>
2025-01-23 23:23:41 +0530

    Type Casting and Promotion

--- Hello.java
+++ Hello.java
@@ -1,10 +1,13 @@
 public class Hello {
+  /**
+   * @param args
+   */
   public static void main(String[] args) {
     //System.out.println("Hello World");
     //System.out.println(3+4);
 
     //Primitive Data Types and variables and literals
-    int num = 21;
+    /*int num = 21;
     byte b = 127;
     short s = 345;
     long l = 6789l;
@@ -13,6 +16,24 @@ public class Hello {
     char c = 'c';
     boolean bool = true;
 
-    double db = 10_00_00_000;
+    double db = 10_00_00_000;*/
+
+    //Casting on PRIMITIVE VALUES
+    byte b = 125;
+    int a = (int) b;
+    byte c = (byte) a;
+    System.out.println(c);
+
+    float f = 5.6f;
+    int d = (int) f;
+    System.out.println(d);
+
+    //Type Promotion
+    byte a1 = 10;
+    byte b1 = 40;
+
+    int result = a1*b1;
+    System.out.println(result);
+
   }
 }
